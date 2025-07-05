511411969735a80e55959643b44177b73160fddc
wolfcoretechnologies <wolfcoretechnologies@gmail.com>
2025-07-05 12:31:53 +0530

    footer remove

--- packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/index.blade.php
@@ -119,14 +119,6 @@
                 </div>
 
                 <!-- Powered By -->
-                <div class="mt-auto">
-                    <div class="border-t bg-white py-2 text-center text-sm dark:border-gray-800 dark:bg-gray-900 dark:text-white">
-                        @lang('admin::app.components.layouts.powered-by.description', [
-                            'bagisto' => '<a class="text-blue-600 hover:underline dark:text-darkBlue" href="https://bagisto.com/en/">Bagisto</a>',
-                            'webkul' => '<a class="text-blue-600 hover:underline dark:text-darkBlue" href="https://webkul.com/">Webkul</a>',
-                        ])
-                    </div>
-                </div>
             </div>
         </div>
 
